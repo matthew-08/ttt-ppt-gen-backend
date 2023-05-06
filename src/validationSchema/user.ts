947ed95f08e50c82a86b2e1ac1f7f48eda5e6391
Matthew <matthewjcrosby1@gmail.com>
2023-05-06 17:24:08 +0700

    added user schema and  password validation

--- src/validationSchema/user.ts
+++ src/validationSchema/user.ts
@@ -1,1 +1,25 @@
-import { z } from 'zod'
+import { infer, z } from 'zod'
+import validator from 'validator'
+
+const UserSchema = z.object({
+    email: z.string().email(),
+    password: z
+        .string()
+        .min(10, 'Password is too short')
+        .refine(
+            (pass) =>
+                validator.isStrongPassword(pass, {
+                    returnScore: false,
+                    minUppercase: 1,
+                    minSymbols: 1,
+                }),
+            {
+                message:
+                    'Password must contain at least 1 upper case letter and 1 symbol',
+            }
+        ),
+})
+
+type User = z.infer<typeof UserSchema>
+
+export { User, UserSchema }
