a18310a0ac21da1efed58b9a2d9fd47c6d60ea84
Matthew <matthewjcrosby1@gmail.com>
2023-05-23 20:51:19 +0700

    [FEAT]: deserialize user middleware

--- src/middleware/deserializeUser.ts
+++ src/middleware/deserializeUser.ts
@@ -1,3 +1,21 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
+import { getTokenFromHeader } from '../utils/getTokenFromHeader'
+import { IncomingHttpHeaders } from 'http'
+import { verifyJwt } from '../utils/jwt'
 
-const deserializeUser = (req: Request, res: Response) => {}
+const deserializeUser = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    if (!req.headers.authorization) {
+        return res.status(400).send('No authorization header')
+    }
+    const headerToken = getTokenFromHeader(req.headers.authorization)
+    const deserializedToken = await verifyJwt(headerToken)
+    if (deserializedToken.expired || !deserializedToken.decoded) {
+        return res.status(401).send('Session expired')
+    }
+}
+
+export { deserializeUser }
